cbd64f8fea6c4f879d2d20d167868e2e85a06002
anjt333 <18620565430@163.com>
2024-11-21 23:12:10 +0800

    Create Space_ranger.sh

---
+++ Rawdata_to_count/Space_ranger.R
@@ -0,0 +1,7 @@
+ spaceranger count --id=A \
+                   --transcriptome=GRCh38-3.0.0 \
+                   --fastqs=/home/Space/fastq_path \
+                   --sample=A \
+                   --image=/home/Space/images/sample.tif \
+                   --slide=SlideA \
+                   --area=A1
